fd0e49e60495d4121140531542fd76447ab8cfa7
webdevyoo <yjk7454@naver.com>
2024-07-15 20:18:15 +0900

    feat: `Read.vue` - 게시글 글 목록 컴포넌트 구현

---
+++ src/components/Read.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <table>
+      <tr>
+        <th>글쓴이</th>
+        <th>제목</th>
+        <th>내용</th>
+      </tr>
+      <tr 
+      :key="index"
+      v-for="(value, index) in data">
+        <td>{{value.writer}}</td>
+        <td>{{value.title}}</td>
+        <td>{{value.content}}</td>
+      </tr>
+    </table>
+    <button @click="write"> 글쓰기 </button>
+  </div>
+</template>
+
+<script>
+import data from '@/data'
+export default {
+  name: 'Read',
+  data () {
+    return {
+      data
+    }
+  },
+  methods: {
+    write() {
+      this.$router.push({
+        path: 'create'
+      })
+    }
+  }
+}
+</script>
