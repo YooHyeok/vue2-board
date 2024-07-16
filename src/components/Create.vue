e00734a49ec0166bb4da7547eb02ab2e74c90ba4
webdevyoo <yjk7454@naver.com>
2024-07-17 08:32:05 +0900

    feat: `Create.vue` - index 여부를 통해 버튼 text 동적 바인딩 및 수정 기능 구현

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -3,7 +3,7 @@
     <input v-model="writer" type="text" placeholder="글쓴이">
     <input v-model="title" type="text" placeholder="제목">
     <textarea v-model="content" placeholder="내용"/>
-    <button @click="write">작성</button>
+    <button @click="index ? update() : write()">{{ index ? "수정" : "작성"}}</button>
   </div>
 
 </template>
@@ -13,16 +13,18 @@ import data from '@/data'
 export default {
   name: 'Create',
   data () {
+    const index = this.$route.params.contentId
     return {
-      data,
-      writer: '',
-      title: '',
-      content: ''
+      // data,
+      writer: index ? data[index].writer : '',
+      title: index ? data[index].title : '',
+      content: index ? data[index].content : '',
+      index
     }
   },
   methods: {
     write() {
-      this.data.push({
+      data.push({
         writer: this.writer,
         title: this.title,
         content: this.content,
@@ -30,6 +32,16 @@ export default {
       this.$router.push({
         path: "/"
       })
+    },
+    update() {
+        data[this.index].writer = this.writer
+        data[this.index].title = this.title
+        data[this.index].content = this.content
+
+        this.$router.push({
+          path: "/"
+        })
+        return;
     }
   }
 }
