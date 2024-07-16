379962e5d36711133dae77864ae7cd075e931556
webdevyoo <yjk7454@naver.com>
2024-07-16 21:49:50 +0900

    feat: `Detail.vue` - 상세페이지 컴포넌트 추가 및 구현

---
+++ src/components/Detail.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div>{{data.writer}}</div>
+    <div>{{data.title}}</div>
+    <div>{{data.content}}</div>
+    <button @click="updateData">수정</button>
+    <button @click="deleteData">삭제</button>
+  </div>
+</template>
+
+<script>
+import data from '@/data'
+  export default {
+    name: 'Detail',
+    data() {
+      const index = this.$route.params.contentId
+      return {
+        data: data[index],
+        index
+      }
+    },
+    methods: {
+      updateData() {
+
+      },
+      deleteData() {
+
+      }
+    }
+  }
+</script>
